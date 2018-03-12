94a803cd00f913eaf9b458f534f250334e26177b
Hamza Shaikh <hamza.shaikh@convonix.com>
2018-03-12 18:25:09 +0530

    haris

--- shnt/routes/web.php
+++ shnt/routes/web.php
@@ -74,9 +74,9 @@ Route::middleware('loggedin')->group(function() {
     Route::get('addmarks', 'staff@getaddmarksform')->name('staff.forms.addmarks');
     Route::post('addmarks', 'staff@addmarks');
 
-    Route::get('class', 'staff@addclass')->name('staff.forms.class')
-    Route::get('allocatefaculties', 'staff@allocatefaculties')->name('staff.forms.marks')
-    Route::get('addmarks', 'staff@addmarks')->name('staff.forms.marks')
+    Route::get('class', 'staff@addclass')->name('staff.forms.class');
+    Route::get('allocatefaculties', 'staff@allocatefaculties')->name('staff.forms.marks');
+    Route::get('addmarks', 'staff@addmarks')->name('staff.forms.marks');
 
 });
 
