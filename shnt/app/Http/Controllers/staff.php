70ef7d51ae3b319261118861783b31239ab6af98
speedforcecompiler <khan.sohail898@gmail.com>
2018-03-12 18:22:00 +0530

    Sohail Commit

--- shnt/app/Http/Controllers/staff.php
+++ shnt/app/Http/Controllers/staff.php
@@ -18,6 +18,10 @@ class staff extends Controller
         return view('staff.forms.addmarks')->with(compact('user'));
     }
 
+    public function allocatefaculties(){
+        
+    }
+
     public function addmarks(Request $r) {
         \DB::table('scores')->where(['form_id'=>$r->input('form_id'), 'course_id'=>$r->input('course_id')])->update([
             'ia1' => $r->input('ia1'),
