2ef5de367fce9ee953829aae395b868082256947
Neel Bhanushali <neal.bhanushali@gmail.com>
2018-03-20 10:43:51 +0530

    marks adding via csv export then import done: neel

--- shnt/resources/views/staff/forms/addinternalmarks.blade.php
+++ shnt/resources/views/staff/forms/addinternalmarks.blade.php
@@ -14,6 +14,7 @@
 
 
 @section('content')
+<input type="file" id="csv" name="csv" style="display: none" accept=".csv" />
 <div class="row">
     <div class="col s12 m12 l12">
         <div class="card-panel">
@@ -34,13 +35,26 @@
                             @foreach($students as $s)
                             <tr>
                                 <td>{{$s->rollnumber}}</td>
-                                <td>{{$s->ia1}}</td>
-                                <td>{{$s->ia2}}</td>
-                                <td>{{$s->tw}}</td>
                                 <td>
-                                    <form action="" method="post">
+                                    <div class="input-field col s12">
+                                        <input form="{{$s->rollnumber}}_{{$s->exam_form_id}}" placeholder="ia1" id="ia1" name="ia1" type="text" value="{{$s->ia1}}">
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="input-field col s12">
+                                        <input form="{{$s->rollnumber}}_{{$s->exam_form_id}}" placeholder="ia2" id="ia2" name="ia2" type="text" value="{{$s->ia2}}">
+                                    </div>   
+                                </td>
+                                <td>
+                                    <div class="input-field col s12">
+                                        <input form="{{$s->rollnumber}}_{{$s->exam_form_id}}" placeholder="tw" id="tw" name="tw" type="text" value="{{$s->tw}}">
+                                    </div>    
+                                </td>
+                                <td>
+                                    <form id="{{$s->rollnumber}}_{{$s->exam_form_id}}" action="" method="post">
                                         {{csrf_field()}}
-                                        <input type="hidden" name="id" value="{{$s->id}}">
+                                        <input type="hidden" name="rollnumber" value="{{$s->rollnumber}}">
+                                        <input type="hidden" name="exam_form_id" value="{{$s->exam_form_id}}">
                                         <button type="button" onclick="editSyllabus(this)" class="btn-floating waves-effect waves-light editbtn">
                                             <i class="material-icons">edit</i>
                                         </button>
@@ -172,12 +186,7 @@
 @section('js')
 @parent
 
-<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
+<script src="/assets/js/jquery.csv.min.js"></script>
 
 
 <script>
@@ -322,27 +331,75 @@
             $('select').material_select();
     }
 
+    function isAPIAvailable() {
+      // Check for the various File API support.
+      if (window.File && window.FileReader && window.FileList && window.Blob) {
+        // Great success! All the File APIs are supported.
+        return true;
+      } else {
+        // source: File API availability - http://caniuse.com/#feat=fileapi
+        // source: <output> availability - http://html5doctor.com/the-output-element/
+        document.writeln('The HTML5 APIs used in this form are only available in the following browsers:<br />');
+        // 6.0 File API & 13.0 <output>
+        document.writeln(' - Google Chrome: 13.0 or later<br />');
+        // 3.6 File API & 6.0 <output>
+        document.writeln(' - Mozilla Firefox: 6.0 or later<br />');
+        // 10.0 File API & 10.0 <output>
+        document.writeln(' - Internet Explorer: Not supported (partial support expected in 10.0)<br />');
+        // ? File API & 5.1 <output>
+        document.writeln(' - Safari: Not supported<br />');
+        // ? File API & 9.2 <output>
+        document.writeln(' - Opera: Not supported');
+        return false;
+      }
+    }
+
+    function handleFileSelect(evt) {
+      var files = evt.target.files; // FileList object
+      var file = files[0];
+        
+      var reader = new FileReader();
+      reader.readAsText(file);
+      reader.onload = function(event){
+        var csv = event.target.result;
+        var data = $.csv.toObjects(csv);
+        
+        for(var i = 0; i < data.length; i++) {
+            var selector = '[name=rollnumber][value='+data[i]['Roll Number']+']';
+            $(selector).closest('tr').find('[name=ia1]').val(data[i]['IA1']);
+            $(selector).closest('tr').find('[name=ia2]').val(data[i]['IA2']);
+            $(selector).closest('tr').find('[name=tw]').val(data[i]['TW']);
+            
+        }
+      };
+      reader.onerror = function(){ alert('Unable to read ' + file.fileName); };
+    }
+
     $(document).ready(function(){
+        if(isAPIAvailable()) {
+            $('#csv').bind('change', handleFileSelect);
+        }
+
         // $('.datatable').DataTable().destroy();
         datatable.destroy();
         $('.datatable').DataTable( {
           dom: 'Bfrtip',
           buttons: [
-              // 'copyHtml5',
-              {
-                  extend: 'csv',
-                  text: 'Export CSV'
-              },
               {
                   text: 'Import CSV',
                   action: function ( e, dt, node, config ) {
-                      console.log(e);
-                      console.log(dt);
-                      console.log(node);
-                      console.log(config);
-                      alert( 'Button activated' );
+                      $('#csv').click();
                   }
-              }
+              },
+              {
+                  extend: 'csv',
+                  text: 'Export CSV',
+                  filename: 'INTERNAL_ASSESSMENT_{{$course->short}}',
+                  exportOptions: {
+                        columns: [0, 1, 2, 3]
+                },
+              },
+              
           ]
       } );
 
@@ -434,10 +491,4 @@
         });
     });
 </script>
-@endsection
-
-@section('css')
-@parent
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.5.1/css/buttons.dataTables.min.css">
 @endsection
